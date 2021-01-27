324311bc5834f941228775eb02f91dbd9b664e35
CORE Programmers <core2062programmers@googlegroups.com>
2021-01-26 18:25:29 -0600

    Fixed small error in auton

--- src/auto/TestRoutine.cpp
+++ src/auto/TestRoutine.cpp
@@ -3,9 +3,7 @@
 TestRoutine::TestRoutine() : COREAuton("Test Routine") {}
 
 void TestRoutine::AddNodes() {
-    testNode = new Node(5, new TurretTrackingAction(TRACKING_ON));
     delayNode = new Node(3, new DelayAction());
-    testNode2 = new Node(5, new TurretTrackingAction(TRACKING_OFF));
     AddFirstNode(testNode);
     testNode->AddNext(testNode2);
     //delayNode->AddNext(testNode2);
