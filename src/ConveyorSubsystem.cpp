b4a1782291fd081946dc94a1fcf1064bc858c878
Aidan Lincicum <aidanlincicum@gmail.com>
2021-02-04 17:27:08 -0600

    Added comments for clarity

--- src/ConveyorSubsystem.cpp
+++ src/ConveyorSubsystem.cpp
@@ -2,21 +2,27 @@
 #include <CORERobotLib.h>
 
 ConveyorSubsystem::ConveyorSubsystem() : m_conveyorMotor(CONVEYOR_PORT),
-                                         m_conveyorForwardSpeed("Conveyor Forward Speed", 0.2),
-                                         m_conveyorReverseSpeed("Conveyor Reverse Speed", -0.2) {
+                                         m_conveyorForwardSpeed("Conveyor Forward Speed", 0.2), 
+                                         //TODO: Change these values to actual numbers based on testing
+                                         m_conveyorReverseSpeed("Conveyor Reverse Speed", -0.2) 
+                                         //TODO: Change these values to actual numbers based on testing 
+                                                                                                {
 
 }
 
 void ConveyorSubsystem::robotInit() {
+    //Initializes the buttons on operator joystick
     operatorJoystick->RegisterButton(COREJoystick::JoystickButton::LEFT_TRIGGER);
     operatorJoystick->RegisterButton(COREJoystick::JoystickButton::LEFT_BUTTON);
 }
 
 void ConveyorSubsystem::teleopInit() {
+    //Initializes the TalonSRX to start not move at start of teleop
     m_conveyorMotor.Set(ControlMode::PercentOutput, 0);
 }
 
 void ConveyorSubsystem::teleop() {
+    //Allows for operator control of the robot
     if (operatorJoystick->GetButton(COREJoystick::JoystickButton::LEFT_TRIGGER)) {
         setMotor(m_conveyorForwardSpeed.Get());
     } else if (operatorJoystick->GetButton(COREJoystick::JoystickButton::LEFT_BUTTON)) {
@@ -27,5 +33,6 @@ void ConveyorSubsystem::teleop() {
 }
 
 void ConveyorSubsystem::setMotor(double speed) {
+    //Sets the conveyor motor to specified speed
     m_conveyorMotor.Set(ControlMode::PercentOutput, speed);
 }
